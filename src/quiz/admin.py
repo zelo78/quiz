4cdb4bc5e21595eec898e51f3d3b47e6c118b4ee
Oleg <Zelo78@yandex.ru>
2022-12-06 15:53:05 +0300

    Add questions and answers

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.admin import TabularInline
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-# Register your models here.
+from quiz.models import Answer, Option, Question
+
+
+class AnswerInlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        right, wrong = 0, 0
+        for form in self.forms:
+            data = form.cleaned_data
+            if data.get("answer"):
+                if data.get("right") is True:
+                    right += 1
+                elif data.get("right") is False:
+                    wrong += 1
+
+        if right == 0:
+            raise ValidationError("Нет правильного ответа")
+
+        if wrong == 0:
+            raise ValidationError("Нет неправильного ответа")
+
+
+class AnswerInlineAdmin(TabularInline):
+    model = Option
+    ordering = ["-right", "answer__text"]
+    extra = 3
+    min_num = 2
+    formset = AnswerInlineFormSet
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ["text"]
+    list_display = ["text"]
+    fields = ["text"]
+    inlines = [AnswerInlineAdmin]
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ["text"]
--- src/quiz/models.py
+++ src/quiz/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Answer(models.Model):
+    class Meta:
+        verbose_name = "ответ"
+        verbose_name_plural = "ответы"
+        ordering = ["text"]
+
+    text = models.CharField("текст ответа", max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.text}"
+
+    def clean(self):
+        self.text = self.text.strip()
+
+
+class Question(models.Model):
+    class Meta:
+        verbose_name = "вопрос"
+        verbose_name_plural = "вопросы"
+
+    text = models.TextField("текст вопроса")
+    options = models.ManyToManyField(Answer, verbose_name="ответ", through="Option")
+
+    def __str__(self):
+        return f"{self.text}"
+
+    def clean(self):
+        self.text = self.text.strip()
+
+
+class Option(models.Model):
+    class Meta:
+        verbose_name = "вариант ответа"
+        verbose_name_plural = "варианты ответов"
+        unique_together = ["question", "answer"]
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, verbose_name="ответ", on_delete=models.CASCADE)
+    right = models.BooleanField("правильность", default=False)
+
+    def __str__(self):
+        return f"{'Правильный' if self.right else 'Неправильный'} ответ '{self.answer.text}'"
